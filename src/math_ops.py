b64f68021d6a017ded2c37faa2c9394ec4ed32b3
sitesh indra <eindsit@emb-f13mlvcg.ki.sw.ericsson.se>
2020-02-05 13:47:51 +0100

    new files

---
+++ src/math_ops.py
@@ -0,0 +1,32 @@
+class Compute_number():
+    """Compute class to compute
+
+    """
+    def __init__(self):
+        """initialize
+        """
+        return self
+    def sum(self,a,b):
+        """SUM
+
+        Args:
+            a: first argument
+            b: second argument
+
+        Returns: sum of a and b
+
+        """
+        self.a= a
+        self.b = b
+        return a +b
+
+    def diff(self, a,b):
+        """ difference of the values
+        Args:
+            a: first arg
+            b: second arg
+
+        Returns: a-b
+
+        """
+        return a-b
